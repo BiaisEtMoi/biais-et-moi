aa633fa7e22037039ad5c57cafa88f4b93788177
Prouil123 <paulrouillard123@gmail.com>
2025-11-24 18:29:07 +0100

    correction phrases de résultats (#5)
    Signed-off-by: Prouil123 <paulrouillard123@gmail.com>

--- src/routes/iat/config/translations.ts
+++ src/routes/iat/config/translations.ts
@@ -1,12 +1,12 @@
 export const translations = {
   fb_strong_Att1WithCatA_Att2WithCatB:
-    "Vos données suggèrent une forte préférence automatique pour categoryB plutôt que categoryA.",
+    "Vos données suggèrent une forte préférence automatique pour associer les categoryB en temps que bons patients plutôt que les categoryA.",
   fb_moderate_Att1WithCatA_Att2WithCatB:
-    "Vos données suggèrent une préférence automatique modérée pour categoryB plutôt que categoryA.",
+    "Vos données suggèrent une préférence modérée automatique pour associer les categoryB en temps que bons patients plutôt que les categoryA.",
   fb_slight_Att1WithCatA_Att2WithCatB:
-    "Vos données suggèrent une faible préférence automatique pour categoryB plutôt que categoryA.",
+     "Vos données suggèrent une faible préférence automatique pour associer les categoryB en temps que bons patients plutôt que les categoryA.",
   fb_equal_CatAvsCatB:
-    "Vos données ne suggèrent aucune préférence automatique entre categoryA et categoryB.",
+    "Vos données ne suggèrent aucune préférence automatique entre les categoryA et les categoryB en temps que bons ou mauvais patients.",
   leftKeyText: 'Appuyez sur "E" pour',
   rightKeyText: 'Appuyez sur "I" pour',
 };
