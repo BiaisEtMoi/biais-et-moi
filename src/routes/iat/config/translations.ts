f8bf12498eede6c83bdb88b6b6bf9978a26f4ad4
Prouil123 <paulrouillard123@gmail.com>
2025-11-24 18:30:16 +0100

    update résultat neutre (#6)
    Signed-off-by: Prouil123 <paulrouillard123@gmail.com>
    Signed-off-by: David <superd001@gmail.com>
    Co-authored-by: David <superd001@gmail.com>

--- src/routes/iat/config/translations.ts
+++ src/routes/iat/config/translations.ts
@@ -6,7 +6,7 @@ export const translations = {
   fb_slight_Att1WithCatA_Att2WithCatB:
      "Vos données suggèrent une faible préférence automatique pour associer les categoryB en temps que bons patients plutôt que les categoryA.",
   fb_equal_CatAvsCatB:
-    "Vos données ne suggèrent aucune préférence automatique entre les categoryA et les categoryB en temps que bons ou mauvais patients.",
+    "Vos données ne suggèrent aucune préférence automatique d'association ente bons et mauvais patients entre les categoryA et les categoryB.",
   leftKeyText: 'Appuyez sur "E" pour',
   rightKeyText: 'Appuyez sur "I" pour',
 };
