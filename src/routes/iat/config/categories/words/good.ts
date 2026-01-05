4785302e122937b4f28428b5c9d55b16a72052cd
Prouil123 <paulrouillard123@gmail.com>
2026-01-05 14:12:31 +0100

    Update good.ts
    Idem précédent, nouveau titre, nouveaux mots, pas ed changement de la constant "good" pour garder le programme comme il est
    
    
    Signed-off-by: Prouil123 <paulrouillard123@gmail.com>

--- src/routes/iat/config/categories/words/good.ts
+++ src/routes/iat/config/categories/words/good.ts
@@ -1,18 +1,18 @@
 export const good = {
-  name: "bon patient",
+  name: "Grave",
   title: {
-    media: { word: "Bon patient" },
+    media: { word: "Grave" },
     css: { color: "#0000FF", "font-size": "2em" },
     height: 4, //Used to position the "Or" in the combined block.
   },
   stimulusMedia: [
     //Stimuli content as PIP's media objects
-    { word: "Patient" },
-    { word: "Calme" },
-    { word: "Poli" },
-    { word: "Agréable" },
-    { word: "Compréhensif" },
-    { word: "Observant" },
+    { word: "Coma" },
+    { word: "Détresse respiratoire" },
+    { word: "Choc hémorragique" },
+    { word: "Accouchement imminent" },
+    { word: "AVC" },
+    { word: "Arrêt cardiaque" },
   ],
   //Stimulus css
   stimulusCss: { color: "#0000FF", "font-size": "1.8em" },
