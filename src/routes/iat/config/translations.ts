52a22e28015998a42d18af58d94e7a1189081f57
Prouil123 <paulrouillard123@gmail.com>
2026-01-05 17:32:41 +0100

    Update translations.ts (#10)
    Signed-off-by: Prouil123 <paulrouillard123@gmail.com>

--- src/routes/iat/config/translations.ts
+++ src/routes/iat/config/translations.ts
@@ -1,12 +1,12 @@
 export const translations = {
   fb_strong_Att1WithCatA_Att2WithCatB:
-    "Vos données suggèrent une forte préférence automatique pour associer les categoryB en temps que bons patients plutôt que les categoryA.",
+    "Vos données suggèrent une forte préférence automatique pour associer les categoryB en temps que patients graves plutôt que les categoryA.",
   fb_moderate_Att1WithCatA_Att2WithCatB:
-    "Vos données suggèrent une préférence modérée automatique pour associer les categoryB en temps que bons patients plutôt que les categoryA.",
+    "Vos données suggèrent une préférence modérée automatique pour associer les categoryB en temps que patients graves plutôt que les categoryA.",
   fb_slight_Att1WithCatA_Att2WithCatB:
-     "Vos données suggèrent une faible préférence automatique pour associer les categoryB en temps que bons patients plutôt que les categoryA.",
+     "Vos données suggèrent une faible préférence automatique pour associer les categoryB en temps que patients graves plutôt que les categoryA.",
   fb_equal_CatAvsCatB:
-    "Vos données ne suggèrent aucune préférence automatique d'association ente bons et mauvais patients entre les categoryA et les categoryB.",
+    "Vos données ne suggèrent aucune préférence automatique d'association entre patients graves et non graves entre les categoryA et les categoryB.",
   leftKeyText: 'Appuyez sur "E" pour',
   rightKeyText: 'Appuyez sur "I" pour',
 };
