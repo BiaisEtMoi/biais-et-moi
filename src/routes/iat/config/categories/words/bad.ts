7ab0625ceafe31524d06bc208a33b7a8f591845a
David <superd001@gmail.com>
2026-01-05 18:35:57 +0100

    better wording

--- src/routes/iat/config/categories/words/bad.ts
+++ src/routes/iat/config/categories/words/bad.ts
@@ -1,18 +1,17 @@
 export const bad = {
-  name: "Non grave",
+  name: "Patient grave",
   title: {
     media: { word: "Grave" },
     css: { color: "#0000FF", "font-size": "2em" },
     height: 4, //Used to position the "Or" in the combined block.
   },
   stimulusMedia: [
-    //Stimuli content as PIP's media objects
-    { word: "Hoquet" },
-    { word: "Brûlures mictionnelles" },
-    { word: "Démangeaisons" },
-    { word: "Otite" },
-    { word: "Toux" },
-    { word: "Constipation" },
+    { word: "Coma" },
+    { word: "Détresse respiratoire" },
+    { word: "Choc hémorragique" },
+    { word: "Accouchement imminent" },
+    { word: "AVC" },
+    { word: "Arrêt cardiaque" },
   ],
   //Stimulus css
   stimulusCss: { color: "#0000FF", "font-size": "1.8em" },
--- src/routes/iat/config/categories/words/good.ts
+++ src/routes/iat/config/categories/words/good.ts
@@ -1,18 +1,17 @@
 export const good = {
-  name: "Grave",
+  name: "Patient non grave",
   title: {
-    media: { word: "Grave" },
+    media: { word: "Non grave" },
     css: { color: "#0000FF", "font-size": "2em" },
     height: 4, //Used to position the "Or" in the combined block.
   },
   stimulusMedia: [
-    //Stimuli content as PIP's media objects
-    { word: "Coma" },
-    { word: "Détresse respiratoire" },
-    { word: "Choc hémorragique" },
-    { word: "Accouchement imminent" },
-    { word: "AVC" },
-    { word: "Arrêt cardiaque" },
+    { word: "Hoquet" },
+    { word: "Brûlures mictionnelles" },
+    { word: "Démangeaisons" },
+    { word: "Otite" },
+    { word: "Toux" },
+    { word: "Constipation" },
   ],
   //Stimulus css
   stimulusCss: { color: "#0000FF", "font-size": "1.8em" },
